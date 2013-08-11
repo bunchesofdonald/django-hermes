35987febf540f230d484c502b46e60193050b237
Chris Pickett <chris.pickett@nerdery.com>
2013-08-11 07:44:57 -0500

    Creates basic CategoryAdmin in hermes.admin

--- hermes/admin.py
+++ hermes/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Category
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("subject", )}
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
