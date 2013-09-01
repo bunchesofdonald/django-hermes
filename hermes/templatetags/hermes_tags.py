154fd6fa4f0d10f00bcd59e50cf82ac2171067ad
Chris Pickett <chris.pickett@gmail.com>
2013-09-01 08:40:11 -0500

    Changes random_posts tag to use random_post_list.html template.

--- hermes/templatetags/hermes_tags.py
+++ hermes/templatetags/hermes_tags.py
@@ -5,7 +5,7 @@ from hermes.models import Post
 register = template.Library()
 
 
-@register.inclusion_tag('hermes/post_list_include.html')
+@register.inclusion_tag('hermes/random_post_list.html')
 def random_posts(path, limit=3):
     return {
         'posts': Post.objects.random(limit=limit),
