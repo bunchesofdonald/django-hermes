696e6b90b0a0130b8d6afbcfd70640ba542cddac
Chris Pickett <chris.pickett@gmail.com>
2013-09-26 07:18:28 -0500

    Adds dates, author info and categories to feed output.

--- hermes/settings.py
+++ hermes/settings.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.utils.feedgenerator import Atom1Feed
 
 SYNDICATION_FEED_TITLE = getattr(settings, 'SYNDICATION_FEED_TITLE', '')
 SYNDICATION_FEED_LINK = getattr(settings, 'SYNDICATION_FEED_LINK', '/')
 SYNDICATION_FEED_DESCRIPTION = getattr(settings, 'SYNDICATION_FEED_DESCRIPTION', '')
+SYNDICATION_FEED_TYPE = getattr(settings, 'SYNDICATION_FEED_TYPE', Atom1Feed)
