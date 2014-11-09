9ffcc48fc52cabe63969f00c9e7830922eb711f2
Chris Pickett <chris.pickett@gmail.com>
2014-11-09 08:54:17 -0800

    Bump to version 1.3.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-hermes',
-    version='1.2.2',
     author='Chris Pickett',
     author_email='chris.pickett@gmail.com',
-    packages=find_packages(),
-    url='https://github.com/bunchesofdonald/django-hermes',
+    description='A light-weight blogging app for Django.',
     license='MIT',
     long_description=open('README.rst').read(),
-    description='A light-weight blogging app for Django.',
+    name='django-hermes',
+    packages=find_packages(),
+    url='https://github.com/bunchesofdonald/django-hermes',
+    version='1.3.0',
 )
