c96952564c7fc8ea3aa860473ede4c8a6392f1a4
laundromat <laundro@gmail.com>
2015-08-19 22:28:09 +0200

    Added __str__ methods for python3 compatibility

--- hermes/models.py
+++ hermes/models.py
@@ -52,6 +52,9 @@ class Category(models.Model):
     def __unicode__(self):
         return u" > ".join([category.title for category in self.hierarchy()])
 
+    def __str__(self):
+        return self.__unicode__()
+
     @models.permalink
     def get_absolute_url(self):
         return ('hermes_category_post_list', (), {
@@ -168,6 +171,9 @@ class Post(TimestampedModel):
 
     def __unicode__(self):
         return self.subject
+
+    def __str__(self):
+        return self.__unicode__()
 
     @models.permalink
     def get_absolute_url(self):
