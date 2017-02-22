3c0656a0f93ae5470ab16d19f5add9aa6bc5ade0
Sym Roe <sym.roe@talusdesign.co.uk>
2017-02-22 10:31:57 +0000

    Django 1.10 test settings

--- testsettings.py
+++ testsettings.py
@@ -17,3 +17,25 @@ USE_TZ = True
 
 ROOT_URLCONF = 'hermes.urls'
 MIDDLEWARE_CLASSES = ()
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'DIRS': [
+            # root('templates'),
+        ],
+        'OPTIONS': {
+            'debug': True,
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }
+]
